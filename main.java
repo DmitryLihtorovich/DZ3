290473e7e1df820b66f9928458c2351184a79ef2
DmitryLihtorovich <70798585+DmitryLihtorovich@users.noreply.github.com>
2022-06-08 11:21:51 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,41 @@
+package lesson8HW;
+
+public class main {
+    public static void main(String[] args) {
+        //Дано натуральное число N. Выведите слово YES, если число N является точной степенью двойки, или слово NO в противном случае.
+        //Операцией возведения в степень пользоваться нельзя!
+        /*Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int step = 2;
+        while ( step < n ) {
+            step = step * 2;
+        }
+            if (step == n) {
+                System.out.println("yes");
+            }
+            else {
+                System.out.println("no");
+            }
+                     */
+        //В первый день спортсмен пробежал 1,5 километра, а затем он каждую неделю увеличивал пробег на 10% от предыдущего значения.
+        // Определите номер недели, на который пробег спортсмена составит не менее 5 километров.
+        /*int week = 1;
+        double dist = 1.5;
+        do {
+            week++;
+            dist = dist*1.10;
+                }
+        while (dist <=5);
+        System.out.println("nedel " + week);
+                 */
+        /*int week = 1;
+        double dist = 1.5;
+        while (dist <=5){
+            dist = dist*1.10;
+            week++;
+        }
+        System.out.println("nedel " + week);
+                 */
+
+    }
+}
